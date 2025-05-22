fc5fe7b5a82dfd7c77dc8c34a6b1e398844f8ea1
cadauxe <ema.cadaux@cs-soprasteria.com>
2025-05-22 17:57:26 +0200

    feat : adding nightosm tests

--- tests/test_nightosm.py
+++ tests/test_nightosm.py
@@ -13,7 +13,7 @@ def test_nightosm_rasterize_radiance(eolabtools_paths: EOLabtoolsTestsPath) -> N
     TO DO
     """
 
-    ###Modifier dynamiquement les fichiers de configs
+    ##Modifier dynamiquement les fichiers de configs
 
     command = [
         f"night_osm_image_registration",
@@ -26,9 +26,9 @@ def test_nightosm_rasterize_radiance(eolabtools_paths: EOLabtoolsTestsPath) -> N
 
     result = subprocess.run(command, capture_output=True, text=True, check=True)
 
-    # compare_files(reference_dir = f"{eolabtools_paths.sunmap_ref}/test_1tile_low_res",
-    #               output_dir = f"{eolabtools_paths.sunmap_outdir}/test_1tile_low_res_lst",
-    #               tool = "SunMapGen")
+    compare_files(reference_dir = f"{eolabtools_paths.nightosm_ref}/TestRasterizeRadiance",
+                  output_dir = f"{eolabtools_paths.nightosm_outdir}/test_nightosm_rasterize_radiance",
+                  tool = "NightOSM")
 
 
 @pytest.mark.ci
@@ -36,25 +36,22 @@ def test_nightosm_rasterize_rgb(eolabtools_paths: EOLabtoolsTestsPath) -> None:
     """
     TO DO
     """
+    ##Modifier dynamiquement les fichiers de configs
+
     command = [
-        f"python",
-        f"src/eolabtools/sun_map_generation/SunMapGenerator.py",
-        f"--digital_surface_model", f"{eolabtools_paths.sunmap_datadir}/test_1tile_low_res/75-2021-0659-6861-LA93-0M50.tif",
-        f"--tiles_file", f"{eolabtools_paths.sunmap_datadir}/test_1tile_low_res/1tile.shp",
-        f"--area", f"Paris",
-        f"--date", f"2024-08-03", f"2024-08-04", f"1",
-        f"--time", f"08:00", f"17:00", f"120",
-        f"--nb_cores", f"32",
-        f"--occ_changes", f"3",
-        f"--output_dir", f"{eolabtools_paths.sunmap_outdir}/test_1tile_low_res_tif/"
+        f"night_osm_image_registration",
+        f"-o", f"{eolabtools_paths.nightosm_outdir}/test_nightosm_rasterize_rgb/",
+        f"--config", f"{eolabtools_paths.nightosm_datadir}/config/config2.yml",
+        f"--osm-config", f"{eolabtools_paths.nightosm_datadir}/osm-config/osm_config_subtracted.yml",
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2-RGB.tif"
     ]
+    print(command)
 
     result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print(result.stderr)
 
-    compare_files(reference_dir = f"{eolabtools_paths.sunmap_ref}/test_1tile_low_res",
-                  output_dir = f"{eolabtools_paths.sunmap_outdir}/test_1tile_low_res_tif",
-                  tool = "SunMapGen")
+    compare_files(reference_dir=f"{eolabtools_paths.nightosm_ref}/TestRasterizeRadiance",
+                  output_dir=f"{eolabtools_paths.nightosm_outdir}/test_nightosm_rasterize_radiance",
+                  tool="NightOSM")
 
 
 @pytest.mark.ci
@@ -62,28 +59,23 @@ def test_nightosm_register_radiance(eolabtools_paths: EOLabtoolsTestsPath) -> No
     """
     TO DO
     """
+    ##Modifier dynamiquement les fichiers de configs
+
     command = [
-        f"python",
-        f"src/eolabtools/sun_map_generation/SunMapGenerator.py",
-        f"--digital_surface_model", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst",
-        f"--tiles_file", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/2tiles.shp",
-        f"--area", f"Paris",
-        f"--date", f"2024-08-31", f"2024-08-31", f"1",
-        f"--time", f"08:00", f"9:00", f"60",
-        f"--nb_cores", f"32",
-        f"--occ_changes", f"3",
-        f"--output_dir", f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res/",
-        f"--save_temp"
+        f"night_osm_image_registration",
+        f"-o", f"{eolabtools_paths.nightosm_outdir}/test_nightosm_rasterize_radiance/",
+        f"--config", f"{eolabtools_paths.nightosm_datadir}/config/config3.yml",
+        f"--osm-config", f"{eolabtools_paths.nightosm_datadir}/osm-config/osm_config_subtracted.yml",
+        f"{eolabtools_paths.nightosm_datadir}/Extrait1/Extract1-Radiance.tif"
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2-RGB.tif"
     ]
+    print(command)
 
     result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print(result.stderr)
 
-    os.remove(f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst")
-
-    compare_files(reference_dir = f"{eolabtools_paths.sunmap_ref}/test_2tiles_low_res",
-                  output_dir = f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res",
-                  tool = "SunMapGen")
+    compare_files(reference_dir=f"{eolabtools_paths.nightosm_ref}/TestRasterizeRadiance",
+                  output_dir=f"{eolabtools_paths.nightosm_outdir}/test_nightosm_rasterize_radiance",
+                  tool="NightOSM")
 
 
 @pytest.mark.ci
@@ -91,54 +83,62 @@ def test_nightosm_register_rgb(eolabtools_paths: EOLabtoolsTestsPath) -> None:
     """
     TO DO
     """
+    ##Modifier dynamiquement les fichiers de configs
+
     command = [
-        f"python",
-        f"src/eolabtools/sun_map_generation/SunMapGenerator.py",
-        f"--digital_surface_model", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst",
-        f"--tiles_file", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/2tiles.shp",
-        f"--area", f"Paris",
-        f"--date", f"2024-08-31", f"2024-08-31", f"1",
-        f"--time", f"08:00", f"9:00", f"60",
-        f"--nb_cores", f"32",
-        f"--occ_changes", f"3",
-        f"--output_dir", f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res/",
-        f"--save_temp"
+        f"night_osm_image_registration",
+        f"-o", f"{eolabtools_paths.nightosm_outdir}/test_nightosm_register_rgb/",
+        f"--config", f"{eolabtools_paths.nightosm_datadir}/config/config4.yml",
+        f"--osm-config", f"{eolabtools_paths.nightosm_datadir}/osm-config/osm_config_subtracted.yml",
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2-RGB.tif"
     ]
+    print(command)
 
     result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print(result.stderr)
 
-    os.remove(f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst")
+    compare_files(reference_dir=f"{eolabtools_paths.nightosm_ref}/TestRegisterRGB",
+                  output_dir=f"{eolabtools_paths.nightosm_outdir}/test_nightosm_register_rgb",
+                  tool="NightOSM")
 
-    compare_files(reference_dir = f"{eolabtools_paths.sunmap_ref}/test_2tiles_low_res",
-                  output_dir = f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res",
-                  tool = "SunMapGen")
+@pytest.mark.ci
+def test_nightosm_vector(eolabtools_paths: EOLabtoolsTestsPath) -> None:
+    """
+    TO DO
+    """
+    ##Modifier dynamiquement les fichiers de configs
 
+    command = [
+        f"night_osm_vector_registration",
+        f"-o", f"{eolabtools_paths.nightosm_outdir}/test_nightosm_vector/",
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2-displacement_grid.tif"
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2_RadiancePeaks.gpkg"
+    ]
+    print(command)
+
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
+
+    compare_files(reference_dir=f"{eolabtools_paths.nightosm_ref}/TestRegisterVector",
+                  output_dir=f"{eolabtools_paths.nightosm_outdir}/test_nightosm_vector",
+                  tool="NightOSM")
 
 @pytest.mark.ci
 def test_nightosm_simple_config(eolabtools_paths: EOLabtoolsTestsPath) -> None:
     """
     TO DO
     """
+    ##Modifier dynamiquement les fichiers de configs
+
     command = [
-        f"python",
-        f"src/eolabtools/sun_map_generation/SunMapGenerator.py",
-        f"--digital_surface_model", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst",
-        f"--tiles_file", f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/2tiles.shp",
-        f"--area", f"Paris",
-        f"--date", f"2024-08-31", f"2024-08-31", f"1",
-        f"--time", f"08:00", f"9:00", f"60",
-        f"--nb_cores", f"32",
-        f"--occ_changes", f"3",
-        f"--output_dir", f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res/",
-        f"--save_temp"
+        f"night_osm_image_registration",
+        f"-o", f"{eolabtools_paths.nightosm_outdir}/test_nightosm_simple_config/",
+        f"--config", f"{eolabtools_paths.nightosm_datadir}/config/config5.yml",
+        f"--osm-config", f"{eolabtools_paths.nightosm_datadir}/osm-config/osm_config_simple.yml",
+        f"{eolabtools_paths.nightosm_datadir}/Extrait2/Extract2-Radiance.tif"
     ]
+    print(command)
 
     result = subprocess.run(command, capture_output=True, text=True, check=True)
-    print(result.stderr)
 
-    os.remove(f"{eolabtools_paths.sunmap_datadir}/test_2tiles_low_res/listing_2tiles.lst")
-
-    compare_files(reference_dir = f"{eolabtools_paths.sunmap_ref}/test_2tiles_low_res",
-                  output_dir = f"{eolabtools_paths.sunmap_outdir}/test_2tiles_low_res",
-                  tool = "SunMapGen")
+    compare_files(reference_dir=f"{eolabtools_paths.nightosm_ref}/TestSimpleConfig",
+                  output_dir=f"{eolabtools_paths.nightosm_outdir}/test_nightosm_simple_config",
+                  tool="NightOSM")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,17 @@
-# import geopandas as gpd
+import geopandas as gpd
 import rasterio
 import numpy as np
 import os
+from glob import glob
 
 import filecmp
 import pandas as pd
 
+def get_all_files(reference_dir):
+    all_files = glob(os.path.join(reference_dir, "**", "*"), recursive=True)
+    # Filter only files (exclude directories)
+    just_files = [f.split(reference_dir + '/')[-1] for f in all_files if os.path.isfile(f)]
+    return just_files
 
 def compare_csv(file1, file2):
     df1 = pd.read_csv(file1)
@@ -36,31 +42,28 @@ def compare_tif(file1, file2, atol=1e-10):
         if not np.allclose(arr1, arr2, atol=atol):
             raise ValueError(f"TIFF pixel values differ.")
 
-    print("TIFF files are identical.")
-    return True
 
+def compare_gpkg(file1, file2):
+    gdf1 = gpd.read_file(file1)
+    gdf2 = gpd.read_file(file2)
 
-# def compare_gpkg(file1, file2):
-#     gdf1 = gpd.read_file(file1)
-#     gdf2 = gpd.read_file(file2)
-#
-#     # Sort to ensure consistent ordering
-#     gdf1 = gdf1.sort_values(by=gdf1.columns.tolist()).reset_index(drop=True)
-#     gdf2 = gdf2.sort_values(by=gdf2.columns.tolist()).reset_index(drop=True)
-#
-#     if gdf1.equals(gdf2):
-#         print("GPKG files are identical.")
-#         return True
-#     else:
-#         raise ValueError(f"GPKG files differ.")
+    # Sort to ensure consistent ordering
+    gdf1 = gdf1.sort_values(by=gdf1.columns.tolist()).reset_index(drop=True)
+    gdf2 = gdf2.sort_values(by=gdf2.columns.tolist()).reset_index(drop=True)
+
+    if gdf1.equals(gdf2):
+        print("GPKG files are identical.")
+        return True
+    else:
+        raise ValueError(f"GPKG files differ.")
 
 
 def compare_files(reference_dir : str, output_dir : str, tool : str):
     """
     TO DO
     """
-    ref_files = sorted(os.listdir(reference_dir))
-    out_test_files = sorted(os.listdir(output_dir))
+    ref_files = get_all_files(reference_dir)
+    out_test_files = get_all_files(output_dir)
 
     if ref_files == out_test_files:
         print("Both directories contain the same filenames.")
@@ -71,15 +74,25 @@ def compare_files(reference_dir : str, output_dir : str, tool : str):
 
     # Compare output files with references
     if tool == "NightOSM":
-        pass
-    # elif tool == "SunMapGen":
-    #     for f in ref_files :
-    #         if ".tif" in f:
-    #             # Compare tif files (pixel value and metadata)
-    #             compare_tif(f"{output_dir}/{f}", f"{reference_dir}/{f}")
-    #         elif ".gpkg" in f:
-    #             # Compare gpkg files
-    #             compare_gpkg(f"{output_dir}/{f}", f"{reference_dir}/{f}")
+        print(ref_files)
+        for f in ref_files :
+            if ".tif" in f:
+                # Compare tif files (pixel value and metadata)
+                compare_tif(f"{output_dir}/{f}", f"{reference_dir}/{f}")
+            elif ".csv" in f :
+                # Compare csv files
+                compare_csv(f"{output_dir}/{f}", f"{reference_dir}/{f}")
+            elif ".gpkg" in f:
+                # Compare gpkg files
+                compare_gpkg(f"{output_dir}/{f}", f"{reference_dir}/{f}")
+    elif tool == "SunMapGen":
+        for f in ref_files :
+            if ".tif" in f:
+                # Compare tif files (pixel value and metadata)
+                compare_tif(f"{output_dir}/{f}", f"{reference_dir}/{f}")
+            elif ".gpkg" in f:
+                # Compare gpkg files
+                compare_gpkg(f"{output_dir}/{f}", f"{reference_dir}/{f}")
     elif tool == "DetecOrCult":
         for f in ref_files:
             if any(ext in f for ext in [".shp", ".dbf", ".prj", ".cpg", ".shx"]):
