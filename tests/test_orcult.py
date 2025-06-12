1f81247db69a767af18243cf2490257c1fc9b81d
cadauxe <ema.cadaux@cs-soprasteria.com>
2025-06-12 14:33:16 +0200

    feat : added normalize test

--- tests/test_orcult.py
+++ tests/test_orcult.py
@@ -91,3 +91,69 @@ def test_plot_orientation_bd_ortho1(eolabtools_paths: EOLabtoolsTestsPath) -> No
     compare_files(reference_dir = f"{eolabtools_paths.plotor_ref}/2023_ortho1",
                   output_dir = f"{eolabtools_paths.plotor_outdir}/2023_ortho1",
                   tool = "DetecOrCult")
+
+
+# @pytest.mark.ci
+# def test_plot_orientation_bd_ortho_dir(eolabtools_paths: EOLabtoolsTestsPath) -> None:
+#     """
+#     TO DO
+#     """
+#     if not os.path.exists(f"{eolabtools_paths.plotor_outdir}/2023_ortho_dir"):
+#         os.makedirs(f"{eolabtools_paths.plotor_outdir}/2023_ortho_dir")
+#
+#     command = [
+#         f"python",
+#         f"src/eolabtools/detection_orientation_culture/detection_orientation_culture/orientation_detection.py",
+#         f"--img", f"{eolabtools_paths.plotor_datadir}/LeHavre_BD_ortho_dir",
+#         f"--type", f"jp2",
+#         f"--rpg", f"{eolabtools_paths.plotor_datadir}/RPG/RPG_LeHavre_2023_ortho1.shp",
+#         f"--output_dir", f"{eolabtools_paths.plotor_outdir}/2023_ortho_dir",
+#         f"--nb_cores", f"4",
+#         f"--patch_size", f"10000",
+#         f"--slope", f"{eolabtools_paths.plotor_datadir}/RGE_ALTI_76_5m_SLOPE_crop_ortho1.tif",
+#         f"--aspect", f"{eolabtools_paths.plotor_datadir}/RGE_ALTI_76_5m_ASPECT_crop_ortho1.tif",
+#         f"--min_nb_line_per_parcel", f"20",
+#         f"--min_len_line", "8",
+#         f"--save_fld",
+#         f"--verbose"
+#     ]
+#     print(" ".join(command))
+#     result = subprocess.run(command, capture_output=True, text=True, check=True)
+#     print(result.stderr)
+#
+#     compare_files(reference_dir = f"{eolabtools_paths.plotor_ref}/2023_ortho_dir",
+#                   output_dir = f"{eolabtools_paths.plotor_outdir}/2023_ortho_dir",
+#                   tool = "DetecOrCult")
+
+
+@pytest.mark.ci
+def test_plot_orientation_bd_ortho1_normalize(eolabtools_paths: EOLabtoolsTestsPath) -> None:
+    """
+    TO DO
+    """
+    if not os.path.exists(f"{eolabtools_paths.plotor_outdir}/2023_ortho1_normalize"):
+        os.makedirs(f"{eolabtools_paths.plotor_outdir}/2023_ortho1_normalize")
+
+    command = [
+        f"python",
+        f"src/eolabtools/detection_orientation_culture/detection_orientation_culture/orientation_detection.py",
+        f"--img", f"{eolabtools_paths.plotor_datadir}/LeHavre_BD_ortho1.jp2",
+        f"--type", f"jp2",
+        f"--rpg", f"{eolabtools_paths.plotor_datadir}/RPG/RPG_LeHavre_2023_ortho1.shp",
+        f"--output_dir", f"{eolabtools_paths.plotor_outdir}/2023_ortho1_normalize",
+        f"--nb_cores", f"4",
+        f"--normalize",
+        f"--slope", f"{eolabtools_paths.plotor_datadir}/RGE_ALTI_76_5m_SLOPE_crop_ortho1.tif",
+        f"--aspect", f"{eolabtools_paths.plotor_datadir}/RGE_ALTI_76_5m_ASPECT_crop_ortho1.tif",
+        f"--min_nb_line_per_parcel", f"20",
+        f"--min_len_line", "8",
+        f"--save_fld",
+        f"--verbose"
+    ]
+    print(" ".join(command))
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    print(result.stderr)
+
+    compare_files(reference_dir = f"{eolabtools_paths.plotor_ref}/2023_ortho1_normalize",
+                  output_dir = f"{eolabtools_paths.plotor_outdir}/2023_ortho1_normalize",
+                  tool = "DetecOrCult")
